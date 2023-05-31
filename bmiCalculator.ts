eaa22fe3cc28e356e8bee13572636f168c9825e5
LukasD <lukas.dvarionis@gmail.com>
2023-05-31 07:59:22 +0300

    9.1 Body mass index

---
+++ bmiCalculator.ts
@@ -0,0 +1,25 @@
+const calculateBmi = (height, weight) => {
+    const heightInMeters = height/100;
+    const bmi = weight/(heightInMeters*heightInMeters)
+    switch (true) {
+        case (bmi < 18.5):
+            return "Underweight (Unhealthy weight)"
+            break
+        case (bmi < 22.9):
+            return "Normal (Healthy weight)"
+            break
+        case (bmi < 24.9):
+            return "Overweight I (Weight at risk)"
+            break
+        case (bmi < 29.9):
+            return "Overweight II (Moderately obese weight)"
+            break
+        case (bmi >= 30):
+            return "Overweight I (Severely obese weight)"
+            break
+        default:
+            return "Unable to calculate bmi"
+    }
+}
+
+console.log(calculateBmi(180, 74))
